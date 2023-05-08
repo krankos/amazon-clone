2b1b15c98eae751dd5f9f3b689ca9f12dca88b63
Khalil Bessaad <bessaadkhalil@gmail.com>
2023-05-08 14:07:55 +0100

    transformed prices

--- scrapper/test.js
+++ scrapper/test.js
@@ -1,4 +1,16 @@
 //read product list from file
 import fs from 'fs';
-const products = JSON.parse(fs.readFileSync('products.json'));
-console.log(products.flat().length);
+const products = JSON.parse(fs.readFileSync('products2.json'));
+// for each product, replace the price with an object containing the price as a float and the currency
+products.forEach(product => {
+    
+    product.price = {
+        // price : $10.25
+        value: product.price?parseFloat(product.price.replace('$', '')):null,
+        currency: 'USD'
+    }
+    console.log(product.price);
+}
+);
+// write the products to a new file
+fs.writeFileSync('products3.json', JSON.stringify(products, null, 2));
