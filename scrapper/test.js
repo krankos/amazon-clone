1d2a6e8f8fa2fa309368439fd5aeaab690011eef
Khalil Bessaad <bessaadkhalil@gmail.com>
2023-05-08 14:48:47 +0100

    changed price mapping

--- scrapper/test.js
+++ scrapper/test.js
@@ -4,11 +4,7 @@ const products = JSON.parse(fs.readFileSync('products2.json'));
 // for each product, replace the price with an object containing the price as a float and the currency
 products.forEach(product => {
     
-    product.price = {
-        // price : $10.25
-        value: product.price?parseFloat(product.price.replace('$', '')):null,
-        currency: 'USD'
-    }
+    product.price? product.price = {amount: parseFloat(product.price.replace("$","")), currency: "USD"} : null;
     console.log(product.price);
 }
 );
